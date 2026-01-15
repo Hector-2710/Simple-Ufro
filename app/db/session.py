bdf1e37a1d1719f416fe5b5c949312751dbff896
hector <h.rosales01@ufromail.cl>
2026-01-15 15:37:21 -0300

    feat: add connects for postgres

--- app/db/session.py
+++ app/db/session.py
@@ -7,7 +7,6 @@ engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
 
 async def init_db():
     async with engine.begin() as conn:
-        # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
 async def get_session() -> AsyncSession:
