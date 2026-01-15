5c11c405b8662ef2d3ff473fca2d34dfc4cc206d
hector <h.rosales01@ufromail.cl>
2026-01-15 16:19:29 -0300

    feat: add endpoint for login

---
+++ app/api/endpoints/login.py
@@ -0,0 +1,30 @@
+from app.services.user_service import user_service
+from app.core import security
+from app.schemas.token import Token
+from app.api.deps import SessionDep
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from fastapi import APIRouter
+from typing import Any
+
+router = APIRouter()
+
+@router.post("/access-token", response_model=Token)
+async def login_access_token(session: SessionDep,form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
+    user = await user_service.authenticate(session, email=form_data.username, password=form_data.password)
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect email or password"
+        )
+    
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    access_token_expires = timedelta(minutes=30)
+    return {
+        "access_token": security.create_access_token(user.id, expires_delta=access_token_expires),
+        "token_type": "bearer",
+    }
