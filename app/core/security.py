007161f311d913a8a243cbe9e9ae2130126a3105
hector <h.rosales01@ufromail.cl>
2026-01-15 15:36:45 -0300

    feat: add security.py for managent passwords and token

--- app/core/security.py
+++ app/core/security.py
@@ -2,9 +2,7 @@ from datetime import datetime, timedelta, timezone
 from typing import Any, Union
 import jwt
 from pwdlib import PasswordHash
-from app.core.config import settings
 
-# pwdlib generic interface
 password_hash = PasswordHash.recommended()
 
 ALGORITHM = "HS256"
