117c12380ae99ca74b53497381a7b583fae533ea
hector <h.rosales01@ufromail.cl>
2026-01-17 00:36:29 -0300

    feat: add class that handle connect and func whit redis

---
+++ app/core/cache.py
@@ -0,0 +1,36 @@
+from typing import Optional, Any
+import redis.asyncio as redis
+import json
+from app.core.config import settings
+
+class CacheManager:
+    client: Optional[redis.Redis] = None
+
+    @classmethod
+    async def connect(cls):
+        cls.client = redis.from_url(settings.REDIS_URL, decode_responses=True)
+        await cls.client.ping()
+        print("✅ Redis Connected")
+
+    @classmethod
+    async def close(cls):
+        if cls.client:
+            await cls.client.close()
+            print("🛑 Redis Closed")
+
+    @classmethod
+    async def get(cls, key: str) -> Optional[Any]:
+        if not cls.client:
+            return None
+        val = await cls.client.get(key)
+        if val:
+            return json.loads(val)
+        return None
+
+    @classmethod
+    async def set(cls, key: str, value: Any, ttl: int = 300):
+        if not cls.client:
+            return
+        await cls.client.set(key, json.dumps(value), ex=ttl)
+
+cache = CacheManager()
