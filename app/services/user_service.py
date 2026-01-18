98bd1e352de83773d6dd6f2c3fe43f65dde95f1e
hector <h.rosales01@ufromail.cl>
2026-01-18 02:13:11 -0300

    feat: now check username and email for create user

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -13,10 +13,20 @@ class UserService:
         return result.scalars().first()
 
     @staticmethod
-    async def create(session: AsyncSession, user_in: UserCreate) -> UserRead:
-        existing_user = await UserService.get_by_email(session, user_in.email)
-        if existing_user:
-            raise UserAlreadyExistsError(user_in.email)
+    async def get_by_username(session: AsyncSession, username: str) -> Optional[User]:
+        statement = select(User).where(User.username == username)
+        result = await session.execute(statement)
+        return result.scalars().first()
+
+    @staticmethod
+    async def create(session: AsyncSession, user_in: UserCreate) -> User:
+        existing_email = await UserService.get_by_email(session, user_in.email)
+        if existing_email:
+            raise UserAlreadyExistsError(f"Email {user_in.email} already registered")
+        
+        existing_username = await UserService.get_by_username(session, user_in.username)
+        if existing_username:
+            raise UserAlreadyExistsError(f"Username {user_in.username} already taken")
 
         db_user = User.model_validate(user_in, update={"hashed_password": security.get_password_hash(user_in.password)})
         session.add(db_user)
