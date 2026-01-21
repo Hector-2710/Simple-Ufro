edf69856e4640567535033eb535c2e355a1c5863
hector <h.rosales01@ufromail.cl>
2026-01-21 18:16:25 -0300

    feat: add depend in router

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -1,6 +1,6 @@
 from app.services.auth_service import auth_service
 from app.schemas.token import Token
-from app.api.deps import SessionDep
+from app.api.deps import SessionDep, LoginRateLimit
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import APIRouter
@@ -9,5 +9,5 @@ from typing import Any
 router = APIRouter()
 
 @router.post("/access-token", response_model=Token)
-async def login_access_token(session: SessionDep, form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
+async def login_access_token(session: SessionDep, rate_limit: LoginRateLimit,form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
     return await auth_service.login(session, identifier=form_data.username, password=form_data.password)
